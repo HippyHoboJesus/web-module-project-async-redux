3a1b5ba5f77194b432386ac91523498821527c9f
Jason Greer <jasemgreer@gmail.com>
2023-08-08 09:28:04 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './App.css';
+import { useEffect } from 'react';
 
-function App() {
+import { getGifs } from './actions';
+
+import GifList from './Components/GifList';
+import GifForm from './Components/GifForm';
+
+function App(props) {
+
+  const { loading, error, getGifs } = props
+
+  useEffect(() => {
+    getGifs('dogs')
+  }, []);
+
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Search for Gifs</h1>
+
+      <GifForm/>
+
+      {
+        (error !== "") && <h3>{error}</h3>
+      }
+
+      {
+        loading ? <h3>We are loading</h3> : <GifList />
+      }
+
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    loading: state.loading,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps, { getGifs })(App);
